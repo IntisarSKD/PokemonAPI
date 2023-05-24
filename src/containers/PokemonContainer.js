cb0cf3fef8784423bd4ddb8c4f4540f850a7318f
Intisar Sikder <sikder.intisar@gmail.com>
2023-05-24 17:10:32 +0100

    removed redundant comments

--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -1,65 +1,6 @@
 // This container will be used to fetch data,
 // manage state and logic related to a particular section
 
-// import React, { useEffect, useState } from 'react';
-// import PokemonCard from '../components/PokemonCard';
-// import PokemonFilter from '../components/PokemonFilter';
-
-// const PokemonContainer = () => {
-//   const [pokemonData, setPokemonData] = useState([]);
-//   const [filteredData, setFilteredData] = useState([]);
-
-//   useEffect(() => {
-//     const fetchPokemonData = async () => {    
-//           const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=905');
-//           const data = await response.json();
-//           const pokemonsWithStats = await Promise.all(
-//             data.results.map(async (pokemon) => {
-//               const res = await fetch(pokemon.url);
-//               const pokemonData = await res.json();
-//               const speciesResponse = await fetch(pokemonData.species.url);
-//               const speciesData = await speciesResponse.json();
-//               const pokedexEntry = speciesData.flavor_text_entries.find(
-//                         (entry) => entry.language.name === 'en'
-//               ).flavor_text;
-//               return {
-//                 ...pokemonData,
-//                 pokedexEntry,
-//               };
-//             })
-//           );
-//           setPokemonData(pokemonsWithStats);
-//           setFilteredData(pokemonsWithStats);
-//       };
-//     fetchPokemonData();
-//   }, []);
-
-//   const handleFilterChange = (value) => {
-//     if (value === '') {
-//       setFilteredData(pokemonData);
-//     } else {
-//       const filtered = pokemonData.filter((pokemon) =>
-//         pokemon.name.toLowerCase().includes(value.toLowerCase())
-//       );
-//       setFilteredData(filtered);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h1>Pokémon List</h1>
-//       <PokemonFilter onFilterChange={handleFilterChange} />
-//       <div>
-//         {filteredData.map((pokemon) => (
-//           <PokemonCard key={pokemon.name} pokemon={pokemon} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PokemonContainer;
-
 import React, { useEffect, useState } from 'react';
 import PokemonCard from '../components/PokemonCard';
 import PokemonFilter from '../components/PokemonFilter';
